bd904af05d641b23cdfa47643e989226b38c9de0
LukasHart <hartlukas93@gmail.com>
2023-02-14 09:24:41 -0800

    Udates to bank_account_v2

--- fundamentals/oop/bank_account_v2.py
+++ fundamentals/oop/bank_account_v2.py
@@ -1,8 +1,16 @@
 class BankAccount:
+    accounts = []
     def __init__(self):
         self.int_rate = 0.19
         self.balance = 0
-        
+        BankAccount.accounts.append(self)
+    
+    
+    @classmethod
+    def all_accounts(cls):
+        for account in cls.accounts:
+            print(f'This accounts interest rate is: {account.int_rate} %, This accounts balance is: {account.balance}')
+    
     def deposit(self,amount):
         self.balance += amount
         print(f"{amount} has been deposited to your account")
@@ -19,7 +27,7 @@ class BankAccount:
         return self
     
     def display_account_info(self):
-        print(f"Your account balance is ${self.balance}\nYour account's interest rate is {self.int_rate}")
+        print(f"Your account balance is ${self.balance}\nYour account's interest rate is ${self.int_rate}")
         return self
     
     def yield_interest(self):
@@ -28,5 +36,7 @@ class BankAccount:
         return self
     
 user_1 = BankAccount()
+user_2 = BankAccount()
 
 user_1.deposit(500).withdraw(250).yield_interest().display_account_info()
+BankAccount.all_accounts()
